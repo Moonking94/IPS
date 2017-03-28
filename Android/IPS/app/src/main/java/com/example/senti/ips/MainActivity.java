db38966522fc30fe1a4f8a17520843e32b848f77
Moonking94 <ianhadi96@gmail.com>
2017-03-28 21:41:09 +0800

    First trial

--- Android/IPS/app/src/main/java/com/example/senti/ips/MainActivity.java
+++ Android/IPS/app/src/main/java/com/example/senti/ips/MainActivity.java
@@ -303,8 +303,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     }
                 }
 
-                System.out.println(params);
-
                 return params;
             }
         };
